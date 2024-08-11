a9dd8feb9b9b8737ecfbe1934d1ed0269955cc50
Divyesh-Kamalanaban <70023106+Divyesh-Kamalanaban@users.noreply.github.com>
2024-08-11 20:44:41 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -14,21 +14,27 @@ gsap.to('.skill-title', {
 var tl = new TimelineMax();
 
 // tl
-gsap.to('.m1', {
-  scrollTrigger: {
-    trigger: '.timeline-circle',
-    start: 'center center',
-  },
-  height: '100%'
-})
-gsap.to('.m2', {
-  scrollTrigger: '.timeline-circle',
-  height: '100%'
-})
+// gsap.to('.m1', {
+//   scrollTrigger: {
+//     trigger: '.timeline-circle',
+//     start: 'center center',
+//   },
+//   height: '100%'
+// })
+		ScrollReveal().reveal('.m1')
+		ScrollReveal().reveal('.tc1', { delay: 500 })
+    ScrollReveal().reveal('.m2')
+		ScrollReveal().reveal('.tc2', { delay: 500 })
+    ScrollReveal().reveal('.m3')
+		ScrollReveal().reveal('.tc3', { delay: 500 })
+// gsap.to('.m2', {
+//   scrollTrigger: '.timeline-circle',
+//   height: '100%'
+// })
 
-gsap.to('.m2', {
-  scrollTrigger: '.m3',
-  height: '100%'
-})
+// gsap.to('.m2', {
+//   scrollTrigger: '.m3',
+//   height: '100%'
+// })
 
 
